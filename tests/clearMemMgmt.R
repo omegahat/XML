7806d5faea5fe2d49df6067444591b12fac15e2e
Duncan Temple Lang <duncan@r-project.org>
2012-08-09 07:58:02 -0700

    ...

--- tests/clearMemMgmt.R
+++ tests/clearMemMgmt.R
@@ -5,6 +5,7 @@ newXMLNode("section", attrs = c(id = "a"), parent = p)
 newXMLNode("section", attrs = c(id = "b"), parent = p)
 
 .Call("R_clearNodeMemoryManagement", p, PACKAGE = "XML")
+rm(p)
 gc()
 gc()
 
