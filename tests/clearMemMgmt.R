18fbd6e163c070fa3ba79df57ce76b32e22ca2c3
Duncan Temple Lang <duncan@r-project.org>
2012-08-08 19:25:23 -0700

    moving files for R structure

---
+++ tests/clearMemMgmt.R
@@ -0,0 +1,11 @@
+library(XML)
+
+p = newXMLNode("top")
+newXMLNode("section", attrs = c(id = "a"), parent = p)
+newXMLNode("section", attrs = c(id = "b"), parent = p)
+
+.Call("R_clearNodeMemoryManagement", p, PACKAGE = "XML")
+gc()
+gc()
+
+
