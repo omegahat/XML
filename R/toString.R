92a3e22fbbbdbd902048a6456add03be07aeeffc
Duncan Temple Lang <duncan@r-project.org>
2023-12-04 11:00:46 -0800

    better version of toString.XMLNode but not exported

--- R/toString.R
+++ R/toString.R
@@ -1,11 +1,11 @@
 toString.XMLNode <-
 function(x, ...)
 {
-  .tempXMLOutput = ""  # Put here for codetools to detect the variable locally.
+  # .tempXMLOutput = ""  # Put here for codetools to detect the variable locally.
   con <- textConnection(".tempXMLOutput", "w", local = TRUE)
+  on.exit(close(con))
   sink(con)
   print(x)
   sink()
-  close(con)
-  paste(.tempXMLOutput, collapse="\n")
-}  
+  paste(textConnectionValue(con), collapse="\n")
+}
